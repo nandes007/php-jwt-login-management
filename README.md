b1a6dfa81b1b561a4bb9ed6d950c71a04a4179e3
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2022-07-04 17:14:07 +0700

    add db and modify README file

--- README.md
+++ README.md
@@ -4,7 +4,23 @@
 - mysql >= 5.7.0
 - firebase/php-jwt >= ^6.2
 
+# Config
+- restore database dari db folder ke mysql
+- database (folder config-database.php):
+  - host: localhost
+    user: root
+    password: 'sesuaikan'
+    database: login_management 
+  - host: localhost
+    user: root
+    password: 'sesuaikan'
+    database: login_management_test (database untuk testing)
+
 # intallation
 - clone project to local directory
 - composer install
 
+# Run
+- Masuk ke directory publi : 'cd public'
+- Jalankan script : 'php -S localhost:8080'
+- atau buat localserver sendiri
---
+++ db/login_management_DB.sql
@@ -0,0 +1,46 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.7.38-log : Database - php_login_management
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`php_login_management` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `php_login_management`;
+
+/*Table structure for table `sessions` */
+
+DROP TABLE IF EXISTS `sessions`;
+
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `_token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
